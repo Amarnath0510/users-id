bb54289619934c0d8fa4693cd76df33cc0d52e87
Amarnath0510 <amarmuthu007@gmail.com>
2021-12-28 18:17:27 +0530

    finished

--- src/App.js
+++ src/App.js
@@ -8,57 +8,63 @@ import Button from '@mui/material/Button';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { useState } from 'react';
 import { useHistory,useParams } from "react-router-dom";
-import { Switch, Route, Link } from "react-router-dom";
-import { EditUsers } from './EditUsers';
+import { Switch, Route } from "react-router-dom";
+// import { EditUsers } from './EditUsers';import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
+import Toolbar from '@mui/material/Toolbar';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
  function App() {
   
   const INITIAL_USERS=[{
-  name:"Username:Amarnath",
-  place:"Place:Vadalur",
-  picture:"https://wallpaperaccess.com/full/2213424.jpg",
-  ambition:"Ambition:Full stack developer",
-  age:"Age:27",
+  name:"Username:George",
+  place:"Place:Sydney",
+  picture:"https://www.teahub.io/photos/full/49-497202_football-players-wallpapers-hd.jpg",
+  ambition:"Ambition:Football-Player",
+  age:"Age:22",
   },
     {
-      name:"Name:Raji",
-      picture:"https://images.unsplash.com/photo-1493225457124-a3eb161ffa5f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c2luZ2VyfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
-      place:"Place:Chennai",
-      ambition:"Ambition:Singer",
+      name:"Name:Robert",
+      picture:"https://media.istockphoto.com/photos/high-school-students-raising-hands-on-a-class-picture-id1177608272?k=20&m=1177608272&s=612x612&w=0&h=F7kr3P89Uw7SuXpbfTN-mHFlXOHtCOjghwV76sxD1vM=",
+      place:"Place:Malaysia",
+      ambition:"Ambition:Professor",
       age:"Age:26",
       
        },
            {
-      name:"Name:Solomon",
-      picture:"https://miro.medium.com/max/1400/0*0fClPmIScV5pTLoE.jpg",
-      place:"Place:Mumbai",
-      ambition:"Ambition:International cricket player",
-      age:"Age:42",
+      name:"Name:Silvester",
+      picture:"https://media.istockphoto.com/photos/focused-young-artist-playing-electric-piano-picture-id888275014?k=20&m=888275014&s=612x612&w=0&h=8HjWcjGJQWeTGsHzJw1-g7EX7xe7AT7pk47daKmkynQ=",
+      place:"Place:Canada",
+      ambition:"Ambition:Music-Composer",
+      age:"Age:25",
        },
           
-    {   name:"Name:Tom",
-    picture:"https://images.squarespace-cdn.com/content/v1/52d4b597e4b086fdf73bc331/1390329989595-6Z392WLP0CCOI89L3KMM/_MG_5933.jpg?format=1000w",
+    {   name:"Name:Catherine",
+    picture:"https://startupmindset.com/wp-content/uploads/2015/01/rsz_driven_female_enterpreneur-780x528.jpg",
       place:"Place:California",
-      ambition:"Ambition:Director",
-      age:"Age:32",
+      ambition:"Ambition:Enterprenuer",
+      age:"Age:21",
       
        },
         {
-      name:"Name:Edwins",
-      picture:"https://i.insider.com/569d1bd8e6183e9d408b9e95?width=1000&format=jpeg&auto=webp",
-      place:"Place:Russia",
-      ambition:"Ambition:Astronaut",
-      age:"Age:35",
+      name:"Name:Elysaa",
+      picture:"https://st2.depositphotos.com/10614052/i/600/depositphotos_441478022-stock-photo-female-doctor-sitting-table-clinic.jpg",
+      place:"Place:spain",
+      ambition:"Ambition:Doctor",
+      age:"Age:25",
        }];
        
-      
+      const history=useHistory();
+      const[mode,setMode]=useState("dark");
        const[name,setName]=useState("");
        const[picture,setPicture]=useState("");
        const[place,setPlace]=useState("");
        const[ambition,setAmbition]  =useState("");
        const[age,setAge]=useState("");
        const [users,setUsers]=useState(INITIAL_USERS);
-       
+     
        const addUser=()=>{
          console.log("Adding Users",name,picture,place,ambition,age);
          const newUser={
@@ -73,20 +79,27 @@ import { EditUsers } from './EditUsers';
        };
      
         
- 
+       const theme = createTheme({
+        palette: {
+          mode: mode,
+        },
+      });
       
   return (
+    <ThemeProvider theme={theme}>
+    
+
     <div className="App  "> 
-    <nav>
-
-    <Link to="/home">
-    Home
-    </Link>
-
-    <Link to="/users">
-    Users
-    </Link>
-   </nav>
+    
+    <AppBar position="static" style={{marginBottom:"20px" }}>
+  <Toolbar variant="dense">
+  <Button onClick={()=>history.push("/home")} variant="text" color="inherit">Home</Button>
+  <Button onClick={()=>history.push("/users")} variant="text" color="inherit">Users</Button>
+  <Button startIcon= {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}onClick={()=>setMode(mode==="dark"? "light": "dark")} style={{marginLeft:"auto"}} variant="text" color="inherit">{mode==="light"?"Dark":"Light"}mode</Button>
+  
+  </Toolbar>
+</AppBar>
+ 
    
    <div className="add-user">
    
@@ -121,16 +134,14 @@ import { EditUsers } from './EditUsers';
      variant="standard"
      />
      
-     <Button className="add-button" onClick={addUser} variant="outlined">Add User</Button>
+     <Button className="add-button" onClick={addUser} variant="outlined" style={{color:"blue"}}>Add User</Button>
     
   </div>
   <Switch>
   <Route path="/home">
   <Home/>
   </Route>
-    <Route  path="/users/edit/:id">
-    <EditUsers/>
-    </Route>
+   
     <Route exact path="/users/:id">
     <UserDetails users={users}/>
     </Route>
@@ -138,12 +149,22 @@ import { EditUsers } from './EditUsers';
     <Route exact path="/users">
     <UsersList users={users} setUsers={setUsers}/>
     </Route>
+    <Route path="**">
+    <NotFound/>
+    </Route>
     </Switch>
 </div>
- 
-    
+
+</ThemeProvider>
   )
     
+}
+function   NotFound() {
+  return(
+    <div>
+    <img src="https://kfg6bckb.media.zestyio.com/yalantis-interactive-404.gif" alt="not found 404" className='notfound'/>
+    </div>
+  )
 }
 function UserDetails({users}){
   const{id}=useParams();
@@ -157,7 +178,7 @@ function Home(){
 return(
   <div className="home">
   <h1 className="front">Hello All !!! Welcome to USers Page</h1>
-    <p className="community">A community to add new users,delete and edit users👨🏽‍🤝‍👨🏽👨🏽‍🤝‍👨🏽</p>
+    <p className="community">A community to add new and delete users👨🏽‍🤝‍👨🏽👨🏽‍🤝‍👨🏽</p>
 
   </div>
 )
@@ -167,7 +188,7 @@ return(
 
 
  function Users({name,place,picture,ambition,age,id,deleteButton,editButton}){
-  const history=useHistory();
+ const history=useHistory();
  
    return(
     
@@ -180,7 +201,7 @@ return(
      <p>{ambition}</p>
      <p>{age}</p>
      {deleteButton}
-     {editButton}
+    
    
     
    
@@ -219,17 +240,16 @@ return(
   <DeleteIcon />
 </IconButton>
   }
-  editButton={  <IconButton   
-    onClick={()=>history.push("/users/edit"+index)}
-    aria-label="edit" color="secondary">
-  < EditIcon />
-</IconButton>
-  } 
+
 
     />
   ))} 
  
   </div>
+   
+   
+
+
   )
 
 }
